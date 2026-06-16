5499c6722d56a84605243834498757fb2e4a90a7
AhmedMD72 <152437380+AhmedMD72@users.noreply.github.com>
2026-06-16 21:17:56 +0300

    Delete rover-gui/rover directory

--- rover-gui/rover/LandingPage.js
+++
@@ -1,9 +0,0 @@
-function startMission() {
-    // Adds a dramatic fade out before navigating
-    document.body.style.transition = "opacity 0.8s ease";
-    document.body.style.opacity = "0";
-    
-    setTimeout(() => {
-        window.location.href = "SelectOperationalMode.html";
-    }, 800);
-}
